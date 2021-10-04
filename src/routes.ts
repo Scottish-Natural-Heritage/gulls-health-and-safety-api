42d09e85d904928ad09107598e5b43a0ad0aff1f
John Moore <john.moore@snh.gov.uk>
2021-10-04 17:50:52 +0100

    Fix error hanling and linting in router
    Issue: Scottish-Natural-Heritage/Licensing#855

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import {ServerRoute, Request, ResponseToolkit} from '@hapi/hapi';
-import PostcodeLookup from './controllers/postcode-lookup-controller';
-import Address from './models/address';
+import PostcodeLookupController from './controllers/postcode-lookup-controller';
+import PostcodeLookup from './models/postcode-lookup';
 /**
  * An array of all the routes and controllers in the app.
  */
@@ -15,17 +15,21 @@ const routes: ServerRoute[] = [
   {
     method: 'get',
     path: `/postcode`,
-    handler: async (_request: Request, h: ResponseToolkit) => {
+    handler: async (request: Request, h: ResponseToolkit) => {
       // Grab the Postcode from the query params.
-      const postcodeQuery: string = _request.query.q as string;
+      const postcodeQuery: string = request.query.q as string;
       // Try to GET the addresses from the Postcode lookup service.
       try {
-        const addressResults: Address[] = await PostcodeLookup.findAddresses(postcodeQuery);
+        const addressResults: PostcodeLookup = await PostcodeLookupController.findAddresses(postcodeQuery);
         // Return the address results back to the calling application.
-        return h.response(addressResults);
+        // If there was addresses that matched a valid postcode then return the address results array
+        // else there was no matching address results from the postcode then return the results array. which contains a text entry 'No records found.'
+        return addressResults.results[0].address
+          ? h.response(addressResults.results[0].address)
+          : h.response(addressResults.results[0]);
       } catch {
         // If something went wrong while trying to find addresses send back a 500 with a error message
-        return h.response(new Error('No Postcode provided')).code(500);
+        return h.response({message: 'Invalid postcode.'}).code(500);
       }
     },
   },
