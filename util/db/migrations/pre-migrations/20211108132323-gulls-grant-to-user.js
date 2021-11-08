334dad972e8c9f00f713b4cc6c33e5a683987131
Paul Cox <paul.cox@nature.scot>
2021-11-08 13:35:24 +0000

    Add pre-migration scripts. Issue Licensing 968

---
+++ util/db/migrations/pre-migrations/20211108132323-gulls-grant-to-user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// The pre-migrations only make sense when running inside the production docker
+// environment. They are not required for the development SQLite DB.
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      // Allow our app's user to connect to the database.
+      await queryInterface.sequelize.query('grant connect on database licensing to gulls;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      // Grant our app's user full control of their own schema.
+      await queryInterface.sequelize.query('grant all on schema gulls to gulls;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+      // Revoke our app's user's privileges from its schema.
+      await queryInterface.sequelize.query('revoke all on schema gulls from gulls;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      // Prevent our app's user from connecting to the database.
+      await queryInterface.sequelize.query('revoke all on database licensing from gulls;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+    }
+  };
+} else {
+  module.exports = {
+    up: () => {
+      return Promise.resolve();
+    },
+    down: () => {
+      return Promise.resolve();
+    }
+  };
+}
