138b3e01a83b27a672f6ee71d82561bc1d924c2f
Paul Cox <paul.cox@nature.scot>
2021-11-09 15:11:25 +0000

    Remove some auto-generated boilerplate I missed. Issue Licensing 968

--- util/db/migrations/20211109142550-gulls-create-measures.js
+++ util/db/migrations/20211109142550-gulls-create-measures.js
@@ -2,27 +2,6 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
-'use strict';
-
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Measures', {
       id: {
         allowNull: false,
