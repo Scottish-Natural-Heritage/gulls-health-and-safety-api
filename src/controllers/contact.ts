4e401c9f555f1ff387bdb699c403b5d29b033ff6
Paul Cox <paul.cox@nature.scot>
2021-11-17 13:19:00 +0000

    Add bulk of easier cleaning functions and outline of endpoint and controller. Issue 954

--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -1,3 +1,4 @@
+import transaction from 'sequelize/types/lib/transaction';
 import database from '../models/index.js';
 
 const {Contact} = database;
@@ -9,6 +10,16 @@ const ContactController = {
 
   findAll: async () => {
     return Contact.findAll();
+  },
+
+  create: async (contact: any) => {
+    const contactId = Math.floor(Math.random() * 99_999);
+    contact.id = contactId;
+    let newContact: any;
+    await database.sequelize.transaction(async (t: transaction) => {
+      newContact = await Contact.create(contact, {transaction: t});
+    });
+    return newContact;
   },
 };
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,115 @@
 import {ServerRoute, Request, ResponseToolkit} from '@hapi/hapi';
 import PostcodeLookupController from './controllers/postcode-lookup-controller';
 import PostcodeLookup from './models/postcode-lookup';
+import utils from 'naturescot-utils';
+import Contact from './controllers/contact';
+
+const cleanOnBehalfContact = (body: any) => {
+  return {
+    name: body.onBehalfDetails.name.trim(),
+    organisation:
+      body.onBehalfDetails.organisation === undefined ? undefined : body.onBehalfDetails.organisation.trim(),
+    emailAddress: utils.formatters.stripAndRemoveObscureWhitespace(body.onBehalfDetails.emailAddress.toLowerCase()),
+    phoneNumber: body.onBehalfDetails.phoneNumber === undefined ? undefined : body.onBehalfDetails.phoneNumber.trim(),
+  };
+};
+
+const cleanLicenseHolderContact = (body: any) => {
+  return {
+    name: body.licenceHolderDetails.name.trim(),
+    organisation:
+      body.licenceHolderDetails.organisation === undefined ? undefined : body.licenceHolderDetails.organisation.trim(),
+    emailAddress: utils.formatters.stripAndRemoveObscureWhitespace(
+      body.licenceHolderDetails.emailAddress.toLowerCase(),
+    ),
+    phoneNumber:
+      body.licenceHolderDetails.phoneNumber === undefined ? undefined : body.licenceHolderDetails.phoneNumber.trim(),
+  };
+};
+
+const cleanAddress = (body: any) => {
+  return {
+    uprn: body.uprn === undefined ? undefined : body.uprn.trim(),
+    postcode: body.postcode.trim(),
+    addressLine1: body.manualAddress?.addressLine1.trim(),
+    addressLine2: body.manualAddress?.addressLine2 === undefined ? undefined : body.manualAddress?.addressLine2.trim(),
+    addressTown: body.manualAddress?.addressTown.trim(),
+    addressCounty: body.manualAddress?.addressCounty.trim(),
+  };
+};
+
+const cleanSiteAddress = (body: any) => {
+  return {
+    uprn: body.siteUprn === undefined ? undefined : body.uprn.trim(),
+    postcode: body.sitePostcode.trim(),
+    addressLine1: body.siteManualAddress?.addressLine1.trim(),
+    addressLine2:
+      body.siteManualAddress?.addressLine2 === undefined ? undefined : body.siteManualAddress?.addressLine2.trim(),
+    addressTown: body.siteManualAddress?.addressTown.trim(),
+    addressCounty: body.siteManualAddress?.addressCounty.trim(),
+  };
+};
+
+const cleanApplication = (body: any) => {
+  return {
+    isResidentialSite: body.isResidentialSite,
+    siteType: body.isResidentialSite ? body.residentialType : body.commercialType,
+    previousLicenceNumber: body.previousLicence ? body.previousLicenceNumber.trim() : undefined,
+    supportingInformation: body.supportingInformation === undefined ? undefined : body.supportingInformation.trim(),
+  };
+};
+
+const cleanIssue = (body: any) => {
+  return {
+    // Just copy across the booleans.
+    aggression: body.issueOnSite.aggression,
+    diveBombing: body.issueOnSite.diveBombing,
+    noise: body.issueOnSite.noise,
+    droppings: body.issueOnSite.droppings,
+    nestingMaterial: body.issueOnSite.nestingMaterial,
+    atHeightAggression: body.issueOnSite.atHeightAggression,
+    other: body.issueOnSite.other,
+    // And clean the remaining strings a little.
+    whenIssue: body.siteIssueDetails.whenIssue.trim(),
+    whoAffected: body.siteIssueDetails.whoAffected.trim(),
+    howAffected: body.siteIssueDetails.howAffected.trim(),
+    otherIssueInformation:
+      body.siteIssueDetails.otherIssueInformation === undefined
+        ? undefined
+        : body.siteIssueDetails.otherIssueInformation.trim(),
+  };
+};
+
+const cleanActivity = (body: any, gullType: string) => {
+  return {
+    removeNests: body.species?.[gullType].activities.removeNests,
+    quantityNestsToRemove: body.species?.[gullType].activities.quantityNestsToRemove
+      ? body.species?.[gullType].activities.quantityNestsToRemove
+      : undefined,
+    eggDestruction: body.species?.[gullType].activities.eggDestruction,
+    quantityNestsWhereEggsDestroyed: body.species?.[gullType].activities.quantityNestsWhereEggsDestroyed
+      ? body.species?.[gullType].activities.quantityNestsWhereEggsDestroyed
+      : undefined,
+    chicksToRescueCentre: body.species?.[gullType].activities.chicksToRescueCentre,
+    quantityChicksToRescue: body.species?.[gullType].activities.quantityChicksToRescue
+      ? body.species?.[gullType].activities.quantityChicksToRescue
+      : undefined,
+    chicksRelocateNearby: body.species?.[gullType].activities.chicksRelocateNearby,
+    quantityChicksToRelocate: body.species?.[gullType].activities.quantityChicksToRelocate
+      ? body.species?.[gullType].activities.quantityChicksToRelocate
+      : undefined,
+    killChicks: body.species?.[gullType].activities.killChicks,
+    quantityChicksToKill: body.species?.[gullType].activities.quantityChicksToKill
+      ? body.species?.[gullType].activities.quantityChicksToKill
+      : undefined,
+    killAdults: body.species?.[gullType].activities.killAdults,
+    quantityAdultsToKill: body.species?.[gullType].activities.quantityAdultsToKill
+      ? body.species?.[gullType].activities.quantityAdultsToKill
+      : undefined,
+  };
+};
+
+
 
 /**
  * An array of all the routes and controllers in the app.
@@ -31,6 +140,24 @@ const routes: ServerRoute[] = [
       } catch {
         // If something went wrong while trying to find addresses send back a 500 with a error message
         return h.response({message: 'Invalid postcode.'}).code(500);
+      }
+    },
+  },
+  {
+    method: 'post',
+    path: `/application`,
+    handler: async (request: Request, h: ResponseToolkit) => {
+      const application = request.payload as any;
+      try {
+        if (application.onBehalf) {
+
+        }
+        const newContact = await Contact.create(request);
+        console.log(newContact);
+        return newContact;
+      } catch (error) {
+        console.log(error);
+        return undefined;
       }
     },
   },
