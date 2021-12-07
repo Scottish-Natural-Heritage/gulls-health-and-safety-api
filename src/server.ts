222e4a1cf7d727f595055c7af103d2d2f5b1680e
Paul Cox <paul.cox@nature.scot>
2021-12-07 07:43:26 +0000

    Add pino to server file. Issue Licensing 1006

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ import routes from './routes';
 
 import config from './config/app';
 
+// Use Pino for logging from Hapi
+import HapiPino from 'hapi-pino';
+import JsonUtils from 'json';
+
 // Start up our micro-app.
 const init = async () => {
   const server = Hapi.server({
@@ -15,13 +19,26 @@ const init = async () => {
 
   server.route(routes);
 
+  // Set up logging. At this time we're only interested in error logging, so
+  // we've disabled all of the automatic server access logs.
+  await server.register({
+    plugin: HapiPino,
+    options: {
+      logPayload: true,
+      logRequestComplete: true,
+      logRequestStart: true,
+      logEvents: false,
+      logRouteTags: false
+    }
+  });
+
   // Start the now fully configured HTTP server.
   await server.start();
-  console.log(`Server listening on http://localhost:3017${config.pathPrefix}.`);
+  server.logger.info(`Server listening on http://localhost:3017${config.pathPrefix}.`);
 };
 
 // Start the micro-app and log any errors.
 init().catch((error) => {
-  console.log(error);
+  console.error(JsonUtils.unErrorJson(error));
   throw error;
 });
