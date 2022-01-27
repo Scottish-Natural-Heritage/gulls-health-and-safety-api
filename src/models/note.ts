dcb33875fc00fb1b161b1188e9423eb6547a8099
John Moore <john.moore@snh.gov.uk>
2022-01-27 12:33:24 +0000

    Add new note model/migration
    Issue: Scottish-Natural-Heritage/Licensing#994

---
+++ src/models/note.ts
@@ -0,0 +1,55 @@
+import {DataTypes, Model, Sequelize} from 'sequelize';
+
+/**
+ * Local interface to hold the Note.
+ */
+interface NoteInterface {
+  id?: number;
+  ApplicationId?: number;
+  note?: string;
+  createdBy?: string;
+}
+
+/**
+ * Build a Note model.
+ *
+ * @param {Sequelize.Sequelize} sequelize A Sequelize connection.
+ * @returns {Sequelize.Model} An Note model.
+ */
+const NoteModel = (sequelize: Sequelize) => {
+  class Note extends Model {
+    public id!: number;
+    public ApplicationId!: number;
+    public note!: string;
+    public createdBy!: string;
+  }
+
+  Note.init(
+    {
+      ApplicationId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      note: {
+        type: DataTypes.TEXT,
+      },
+      createdBy: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Note',
+      timestamps: true,
+      paranoid: true,
+    },
+  );
+
+  return Note;
+};
+
+export {NoteModel as default};
+export {NoteInterface};
