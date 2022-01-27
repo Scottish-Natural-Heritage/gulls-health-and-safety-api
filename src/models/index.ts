769b05ce7edda16d29984ccdd32d4ccc57923671
John Moore <john.moore@snh.gov.uk>
2022-01-27 12:34:41 +0000

    Add note model relations
    Issue: Scottish-Natural-Heritage/Licensing#994

--- src/models/index.ts
+++ src/models/index.ts
@@ -16,6 +16,7 @@ import LicenseAdvisory from './license-advisory';
 import LicenseCondition from './license-condition';
 import Advisory from './advisory';
 import Condition from './condition';
+import Note from './note';
 
 const Sequelize = require('sequelize');
 
@@ -44,6 +45,7 @@ const database = {
   LicenseCondition: LicenseCondition(sequelize),
   Advisory: Advisory(sequelize),
   Condition: Condition(sequelize),
+  Note: Note(sequelize),
 };
 
 // Relationships go here.
@@ -65,11 +67,13 @@ database.Assessment.belongsTo(database.Application, {as: 'ApplicationAssessment'
 database.License.belongsTo(database.Application, {as: 'License', foreignKey: 'ApplicationId'});
 database.Issue.belongsTo(database.Application, {as: 'ApplicationIssue', foreignKey: 'ApplicationId'});
 database.Measure.belongsTo(database.Application, {as: 'ApplicationMeasure', foreignKey: 'ApplicationId'});
+database.Note.belongsTo(database.Application, {as: 'ApplicationNotes', foreignKey: 'ApplicationId'});
 
 database.Application.hasOne(database.Assessment, {as: 'ApplicationAssessment', foreignKey: 'ApplicationId'});
 database.Application.hasOne(database.License, {as: 'License', foreignKey: 'ApplicationId'});
 database.Application.hasOne(database.Issue, {as: 'ApplicationIssue', foreignKey: 'ApplicationId'});
 database.Application.hasOne(database.Measure, {as: 'ApplicationMeasure', foreignKey: 'ApplicationId'});
+database.Application.hasMany(database.Note, {as: 'ApplicationNotes', foreignKey: 'ApplicationId'});
 
 database.LicenseAdvisory.belongsTo(database.License, {as: 'LicenseAdvisories', foreignKey: 'LicenseId'});
 database.LicenseCondition.belongsTo(database.License, {as: 'LicenseConditions', foreignKey: 'LicenseId'});
