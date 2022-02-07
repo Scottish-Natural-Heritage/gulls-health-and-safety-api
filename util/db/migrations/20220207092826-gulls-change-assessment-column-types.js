eefc2face8f203ac9d3ed119ae4e8543f89d29c2
John Moore <john.moore@snh.gov.uk>
2022-02-07 12:45:15 +0000

    Change column type to text
    Issue: Scottish-Natural-Heritage/Licensing#1065

---
+++ util/db/migrations/20220207092826-gulls-change-assessment-column-types.js
@@ -0,0 +1,49 @@
+/* eslint-disable unicorn/prefer-module */
+const databaseConfig = require('../database.js');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Assessments',
+      },
+      'testOneAssessment',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Assessments',
+      },
+      'testTwoAssessment',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Assessments',
+      },
+      'testOneAssessment',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Assessments',
+      },
+      'testTwoAssessment',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+  },
+};
