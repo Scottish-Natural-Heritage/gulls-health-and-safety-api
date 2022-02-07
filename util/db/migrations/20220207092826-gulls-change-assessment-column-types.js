aef3e8141a9bef64ade1c72bbb211f9797b15729
John Moore <john.moore@snh.gov.uk>
2022-02-07 14:25:49 +0000

    Add additional columns
    Issue: Scottish-Natural-Heritage/Licensing#1065

--- util/db/migrations/20220207092826-gulls-change-assessment-column-types.js
+++ util/db/migrations/20220207092826-gulls-change-assessment-column-types.js
@@ -33,6 +33,96 @@ module.exports = {
         type: Sequelize.TEXT,
       },
     );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Applications',
+      },
+      'supportingInformation',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'whenIssue',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'whoAffected',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'howAffected',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'otherIssueInformation',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Measures',
+      },
+      'measuresTriedDetail',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Measures',
+      },
+      'measuresWillNotTryDetail',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Revocations',
+      },
+      'reason',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Withdrawals',
+      },
+      'reason',
+      {
+        type: Sequelize.TEXT,
+      },
+    );
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn(
@@ -61,6 +151,96 @@ module.exports = {
         tableName: 'Assessments',
       },
       'refusalReason',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Applications',
+      },
+      'supportingInformation',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'whenIssue',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'whoAffected',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'howAffected',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Issues',
+      },
+      'otherIssueInformation',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Measures',
+      },
+      'measuresTriedDetail',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Measures',
+      },
+      'measuresWillNotTryDetail',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Revocations',
+      },
+      'reason',
+      {
+        type: Sequelize.STRING,
+      },
+    );
+    await queryInterface.changeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Withdrawals',
+      },
+      'reason',
       {
         type: Sequelize.STRING,
       },
