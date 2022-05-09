d085c9472f0c9282501874f0d3c0a8d7d8164410
Paul Cox <paul.cox@nature.scot>
2022-05-09 13:20:10 +0100

    Add migrations scripts to create the required amendment tables. Issue Licensing 1254

---
+++ util/db/migrations/20220509115332-gulls-create-amendments-table.js
@@ -0,0 +1,49 @@
+/* eslint-disable unicorn/prefer-module */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Amendments', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      LicenceId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Licenses',
+          key: 'ApplicationId',
+        },
+      },
+      SpeciesId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'ReturnSpecies',
+          key: 'id',
+        },
+      },
+      amendReason: {
+        type: Sequelize.STRING,
+      },
+      amendedBy: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Amendments');
+  },
+};
