1b29d7914ee44023e2f7a0ef2a3ce317a49ab910
Paul Cox <paul.cox@nature.scot>
2022-05-23 12:18:20 +0100

    Add start of email personalisation code for amendments. Issue Licensing 1255

--- src/controllers/amendment.ts
+++ src/controllers/amendment.ts
@@ -1,5 +1,6 @@
 import transaction from 'sequelize/types/lib/transaction';
 import database from '../models/index.js';
+import ApplicationController from './application';
 import {AdvisoryInterface} from '../models/advisory.js';
 import {ConditionInterface} from '../models/condition.js';
 import config from '../config/app';
@@ -27,6 +28,127 @@ interface AmendmentInterface {
 }
 
 /**
+ * This function returns a summary address built from the address fields of an address object.
+ *
+ * @param {any} fullAddress The address to use to build the summary address from.
+ * @returns {string} Returns a string containing the summary address.
+ */
+ const createSummaryAddress = (fullAddress: any): string => {
+  const address = [];
+  address.push(fullAddress.addressLine1.trim());
+  // As addressLine2 is optional we need to check if it exists.
+  if (fullAddress.addressLine2) {
+    address.push(fullAddress.addressLine2.trim());
+  }
+
+  address.push(fullAddress.addressTown.trim(), fullAddress.addressCounty.trim(), fullAddress.postcode.trim());
+
+  return address.join(', ');
+};
+
+
+// Create a more user friendly displayable date from a date object.
+const createDisplayDate = (date: Date) => {
+  return date.toLocaleDateString('en-GB', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+};
+
+/**
+ * This function creates a formatted string of species and the activities permitted
+ * in the licence to be issued.
+ *
+ * @param {any} species The species the activities pertain to.
+ * @returns {string} Returns a string with all species and licenced activities.
+ */
+ const createPermittedActivitiesList = (species: any): string => {
+  const permittedActivities = [];
+
+  permittedActivities.push('Numbers permitted for amended activities are the total for the site, not additional to those already permitted.');
+
+  if (species.HerringGullId) {
+    permittedActivities.push(addActivities(species.AHerringGull, 'Herring gull'));
+  }
+
+  if (species.BlackHeadedGullId) {
+    permittedActivities.push(addActivities(species.ABlackHeadedGull, 'Black-headed gull'));
+  }
+
+  if (species.CommonGullId) {
+    permittedActivities.push(addActivities(species.ACommonGull, 'Common gull'));
+  }
+
+  if (species.GreatBlackBackedGullId) {
+    permittedActivities.push(addActivities(species.AGreatBlackBackedGull, 'Great black-backed gull'));
+  }
+
+  if (species.LesserBlackBackedGullId) {
+    permittedActivities.push(addActivities(species.ALesserBlackBackedGull, 'Lesser black-backed gull'));
+  }
+
+  return permittedActivities.join('\n');
+};
+
+/**
+ * This function adds the activities to a supplied species string.
+ *
+ * @param {any} species The species for which the activities to be licenced pertains.
+ * @param {string} speciesType A string representing the species name.
+ * @returns {string} Returns a formatted string of all activities for the given species.
+ */
+const addActivities = (species: any, speciesType: string): string => {
+  const activities: string[] = [];
+  if (species.removeNests) {
+    activities.push(
+      `${speciesType}: To take and destroy ${String(
+        species.quantityNestsToRemove,
+      )} nests and any eggs they contain by hand.`,
+    );
+  }
+
+  if (species.eggDestruction) {
+    activities.push(
+      `${speciesType}: To take and destroy eggs from ${String(
+        species.quantityNestsWhereEggsDestroyed,
+      )} nests by oiling, pricking or replacing with dummy eggs.`,
+    );
+  }
+
+  if (species.chicksToRescueCentre) {
+    activities.push(
+      `${speciesType}: To take ${String(
+        species.quantityChicksToRescue,
+      )} chicks to a wildlife rescue centre by hand, net or trap.`,
+    );
+  }
+
+  if (species.chicksRelocateNearby) {
+    activities.push(
+      `${speciesType}: To take ${String(
+        species.quantityChicksToRelocate,
+      )} chicks and relocate nearby by hand, net or trap.`,
+    );
+  }
+
+  if (species.killChicks) {
+    activities.push(
+      `${speciesType}: To kill up to ${String(
+        species.quantityChicksToKill,
+      )} chicks by shooting or by hand, net or trap.`,
+    );
+  }
+
+  if (species.killAdults) {
+    activities.push(
+      `${speciesType}: To kill up to ${String(
+        species.quantityAdultsToKill,
+      )} adults by shooting, falconry or by hand, net or trap.`,
+    );
+  }
+
+  return activities.join('\n');
+};
+
+
+/**
  * This function calls the Notify API and asks for an email to be sent to the supplied address.
  *
  * @param {any} emailAddress The email address to send the email to.
@@ -39,6 +161,22 @@ const sendAmendedEmail = async (emailAddress: string) => {
     });
   }
 };
+
+const setAmendEmailPersonalisationFields = (application: any) => {
+  return {
+    licenceNumber: application.id,
+    siteAddress: createSummaryAddress(application.SiteAddress),
+    startDate: createDisplayDate(application.License.periodFrom),
+    endDate: createDisplayDate(application.License.periodTo),
+    licenceHolderName: application.LicenceHolder.name,
+    licenceHolderAddress: createSummaryAddress(application.LicenceHolderAddress),
+    permittedActivities: createPermittedActivitiesList(application.License.Amendment.ASpecies)
+    licenceConditions:
+    advisoryNotes:
+    statementReason:
+    conservationStatus:
+  }
+}
 
 const AmendmentController = {
   /**
@@ -213,9 +351,13 @@ const AmendmentController = {
       await Note.create(amendmentNote, {transaction: t});
     });
 
+    // We need the licence application details to create the amendment email.
+    const application = (await ApplicationController.findOne(incomingAmendment.LicenceId)) as any;
+
     // Send Notify email if we have a licence holder email address.
     if (incomingAmendment.licenceHolderEmailAddress) {
-      await sendAmendedEmail(incomingAmendment.licenceHolderEmailAddress);
+      const emailDetails = setAmendEmailPersonalisationFields(application)
+      await sendAmendedEmail(incomingAmendment.licenceHolderEmailAddress, emailDetails);
     }
 
     // Send Notify email if we have a licence applicant email address.
