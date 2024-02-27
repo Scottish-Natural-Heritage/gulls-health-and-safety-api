7ec6d7f3e756242bd6b2b8a2c8d5a9c86096d029
Jen Foster <jennifer.massie@nature.scot>
2024-02-27 17:15:45 +0000

    Updated the migration files to include an update and another test with a date change. Scottish-Natural-Heritage/Licensing#2057

---
+++ util/db/migrations/20240227120322-replace-advisory.js
@@ -0,0 +1,47 @@
+/* eslint-disable unicorn/prefer-module */
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      const updatedAdvisory1 =
+        "This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of the licence may constitute an offence. It is the responsibility of the licence holder, agents and assistants working under this licence to report to NatureScot any breaches of the licence conditions that they become aware of as soon as is reasonably possible.";
+
+      await queryInterface.sequelize.query(`UPDATE gulls."Advisories" SET "advisory" = ? WHERE "id" = 3;`, {
+        replacements: [updatedAdvisory1],
+        type: Sequelize.QueryTypes.UPDATE,
+      });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+      const updatedAdvisory1 =
+        'This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of this licence may constitute an offence.';
+
+      await queryInterface.sequelize.query(`UPDATE gulls."Advisories" SET "advisory" = ? WHERE "id" = 3;`, {
+        replacements: [updatedAdvisory1],
+        type: Sequelize.QueryTypes.UPDATE,
+      });
+    },
+  };
+} else {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      const updatedAdvisory1 =
+        "This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of the licence may constitute an offence. It is the responsibility of the licence holder, agents and assistants working under this licence to report to NatureScot any breaches of the licence conditions that they become aware of as soon as is reasonably possible.";
+
+      await queryInterface.sequelize.query(`UPDATE Advisories SET advisory = ? WHERE id = 3;`, {
+        replacements: [updatedAdvisory1],
+        type: Sequelize.QueryTypes.UPDATE,
+      });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+      const updatedAdvisory1 =
+        'This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of this licence may constitute an offence.';
+
+      await queryInterface.sequelize.query(`UPDATE Advisories SET advisory = ? WHERE id = 3;`, {
+        replacements: [updatedAdvisory1],
+        type: Sequelize.QueryTypes.UPDATE,
+      });
+    },
+  };
+}
