7ef1d12c168d6cb8ee69c89f574f67ec9370cd9d
Jen Foster <jennifer.massie@nature.scot>
2024-02-28 07:10:44 +0000

    Removed linting issues. Scottish-Natural-Heritage/Licensing#2057

--- util/db/migrations/20240227120322-replace-advisory.js
+++ util/db/migrations/20240227120322-replace-advisory.js
@@ -3,8 +3,9 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     up: async (queryInterface, Sequelize) => {
+      // prettier-ignore
       const updatedAdvisory1 =
-        "This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of the licence may constitute an offence. It is the responsibility of the licence holder, agents and assistants working under this licence to report to NatureScot any breaches of the licence conditions that they become aware of as soon as is reasonably possible.";
+        'This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of the licence may constitute an offence. It is the responsibility of the licence holder, agents and assistants working under this licence to report to NatureScot any breaches of the licence conditions that they become aware of as soon as is reasonably possible.';
 
       await queryInterface.sequelize.query(`UPDATE gulls."Advisories" SET "advisory" = ? WHERE "id" = 3;`, {
         replacements: [updatedAdvisory1],
@@ -25,8 +26,9 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = {
     up: async (queryInterface, Sequelize) => {
+      // prettier-ignore
       const updatedAdvisory1 =
-        "This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of the licence may constitute an offence. It is the responsibility of the licence holder, agents and assistants working under this licence to report to NatureScot any breaches of the licence conditions that they become aware of as soon as is reasonably possible.";
+        'This licence is granted subject to compliance with the conditions as specified. Anything done otherwise than in accordance with the terms of the licence may constitute an offence. It is the responsibility of the licence holder, agents and assistants working under this licence to report to NatureScot any breaches of the licence conditions that they become aware of as soon as is reasonably possible.';
 
       await queryInterface.sequelize.query(`UPDATE Advisories SET advisory = ? WHERE id = 3;`, {
         replacements: [updatedAdvisory1],
