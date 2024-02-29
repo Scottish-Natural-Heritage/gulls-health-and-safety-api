272cc1478960a608eacc115f71c4417b217e8370
jmassie <jennifer.massie@snh.gov.uk>
2024-02-29 11:32:00 +0000

    Fix/2057 new note (#149)
    * Added new advisory to the database. Scottish-Natural-Heritage/Licensing#2057
    
    * Updated the migration files to include an update and another test with a date change. Scottish-Natural-Heritage/Licensing#2057
    
    * Removed linting issues. Scottish-Natural-Heritage/Licensing#2057
    
    * Updated migration file to delete rather than drop Table following PR review. Scottish-Natural-Heritage/Licensing#2057
    
    * Altered time on migration file to test file works. Scottish-Natural-Heritage/Licensing#2057
    
    ---------
    
    Co-authored-by: Jen Foster <jennifer.massie@nature.scot>

---
+++ util/db/migrations/20240227170746-add-advisory.js
@@ -0,0 +1,23 @@
+/* eslint-disable unicorn/prefer-module */
+module.exports = {
+  up: async (queryInterface) => {
+    // prettier-ignore
+    await queryInterface.bulkInsert('Advisories', [
+      {
+        id: 9,
+        advisory:
+          'It is the responsibility of the licence holder to ensure that all contact details are up to date. Not advising NatureScot of changes to licence holder contact details could result in the licence being revoked.',
+        orderNumber: 9,
+        default: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    // prettier-ignore
+    // eslint-disable-next-line prefer-destructuring
+    const Op = Sequelize.Op
+    await queryInterface.bulkDelete('Advisories', {id: {[Op.in]: [9]}}, {truncate: true});
+  },
+};
