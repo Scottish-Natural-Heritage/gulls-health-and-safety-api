4a5f0fc781ab3a3dd4af68603894154f80707915
Paul Cox <paul.cox@nature.scot>
2025-01-08 12:32:25 +0000

    Add code needed to allow Sequelize to perform initial DB configuration on an RDS. Issue https://github.com/Scottish-Natural-Heritage/AWS-Cloud-Infrastructure-Review-Optimise-Standardise/issues/16

--- util/db/pre-migrations/20220331101421-change-ro-gulls-password.js
+++ util/db/pre-migrations/20220331101421-change-ro-gulls-password.js
@@ -5,16 +5,24 @@ const config = require('../database.js').ssDatabase;
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     up: async (queryInterface, Sequelize) => {
-      return queryInterface.sequelize.query('ALTER ROLE rogulls WITH PASSWORD :roGullsPassword;', {
+      await queryInterface.sequelize.query('ALTER ROLE rogulls WITH PASSWORD :roGullsPassword;', {
         type: Sequelize.QueryTypes.RAW,
         replacements: {
           roGullsPassword: config.password,
         },
       });
+
+      await queryInterface.sequelize.query('revoke gulls from licensing;', {
+        type: Sequelize.QueryTypes.RAW
+      });
     },
 
     down: async (queryInterface, Sequelize) => {
-      return queryInterface.sequelize.query("ALTER ROLE rogulls WITH PASSWORD 'override_this_value';", {
+      await queryInterface.sequelize.query('grant gulls to licensing;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query("ALTER ROLE rogulls WITH PASSWORD 'override_this_value';", {
         type: Sequelize.QueryTypes.RAW,
       });
     },
