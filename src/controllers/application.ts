7454227310192b272429eaed895e44e1386ac015
Rory Anderson <rory.anderson@nature.scot>
2025-02-06 10:08:49 +0000

    Add paranoid variable setting to switch case, hides soft deleted records if true, this was causing the issue with revoked applications displaying incorrectly.

--- src/controllers/application.ts
+++ src/controllers/application.ts
@@ -646,9 +646,11 @@ const ApplicationController = {
     };
 
     let statusObject;
+    let paranoid = false;
 
     switch (status) {
       case 'unassigned':
+        paranoid = true;
         statusObject = {
           $confirmedByLicenseHolder$: true,
           $staffNumber$: {[Op.is]: null},
@@ -658,6 +660,7 @@ const ApplicationController = {
 
         break;
       case 'inProgress':
+        paranoid = true;
         statusObject = {
           $confirmedByLicenseHolder$: true,
           $staffNumber$: {[Op.not]: null},
@@ -666,9 +669,11 @@ const ApplicationController = {
         };
         break;
       case 'awaitingApproval':
+        paranoid = true;
         statusObject = {$confirmedByLicenseHolder$: false};
         break;
       case 'myApplications':
+        paranoid = true;
         statusObject = {
           $confirmedByLicenseHolder$: true,
           $staffNumber$: {
@@ -684,7 +689,7 @@ const ApplicationController = {
     }
 
     return Application.findAndCountAll({
-      paranoid: false,
+      paranoid,
       ...(status === 'unassigned' && {
         attributes: {
           include: [[fn('COALESCE', col('Application.confirmedAt'), col('Application.createdAt')), 'sortByDate']],
