389ff7de12aed370daa2a5c865e94a9bad2710f9
Shazreen Seatter <71439994+ShazSeatter@users.noreply.github.com>
2025-02-06 12:05:27 +0000

    Drop isResidentialType and siteType columns from Applications table. No longer being referened anywhere. Issue: Scottish-Natural-Heritage/Licensing#2691

---
+++ util/db/migrations/20250205145201-remove-sitetype-isresidential-column.js
@@ -0,0 +1,41 @@
+/* eslint-disable unicorn/prefer-module */
+const databaseConfig = require('../database.js');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Applications',
+      },
+      'isResidentialSite',
+    );
+
+    await queryInterface.removeColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Applications',
+      },
+      'siteType',
+    );
+  },
+  down: async (queryInterface) => {
+    await queryInterface.addColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Applications',
+      },
+      'isResidentialSite',
+      Sequelize.BOOLEAN,
+    );
+
+    await queryInterface.addColumn(
+      {
+        schema: databaseConfig.database.schema,
+        tableName: 'Applications',
+      },
+      'siteType',
+      Sequelize.STRING,
+    );
+  },
+};
