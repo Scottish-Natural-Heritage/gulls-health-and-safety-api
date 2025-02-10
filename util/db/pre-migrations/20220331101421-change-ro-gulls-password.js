755274c3d5d3595a301c2adfc7a2647cd1002b86
David Carboni <david@carboni.io>
2025-02-10 16:50:11 +0000

    We can now run Postgres pre-migrations in all environments

--- util/db/pre-migrations/20220331101421-change-ro-gulls-password.js
+++ util/db/pre-migrations/20220331101421-change-ro-gulls-password.js
@@ -2,38 +2,27 @@
 
 const config = require('../database.js').ssDatabase;
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    up: async (queryInterface, Sequelize) => {
-      await queryInterface.sequelize.query('ALTER ROLE rogulls WITH PASSWORD :roGullsPassword;', {
-        type: Sequelize.QueryTypes.RAW,
-        replacements: {
-          roGullsPassword: config.password,
-        },
-      });
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query('ALTER ROLE rogulls WITH PASSWORD :roGullsPassword;', {
+      type: Sequelize.QueryTypes.RAW,
+      replacements: {
+        roGullsPassword: config.password,
+      },
+    });
 
-      await queryInterface.sequelize.query('revoke gulls from licensing;', {
-        type: Sequelize.QueryTypes.RAW,
-      });
-    },
+    await queryInterface.sequelize.query('revoke gulls from licensing;', {
+      type: Sequelize.QueryTypes.RAW,
+    });
+  },
 
-    down: async (queryInterface, Sequelize) => {
-      await queryInterface.sequelize.query('grant gulls to licensing;', {
-        type: Sequelize.QueryTypes.RAW,
-      });
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query('grant gulls to licensing;', {
+      type: Sequelize.QueryTypes.RAW,
+    });
 
-      await queryInterface.sequelize.query("ALTER ROLE rogulls WITH PASSWORD 'override_this_value';", {
-        type: Sequelize.QueryTypes.RAW,
-      });
-    },
-  };
-} else {
-  module.exports = {
-    up: () => {
-      return Promise.resolve();
-    },
-    down: () => {
-      return Promise.resolve();
-    },
-  };
-}
+    await queryInterface.sequelize.query("ALTER ROLE rogulls WITH PASSWORD 'override_this_value';", {
+      type: Sequelize.QueryTypes.RAW,
+    });
+  },
+};
