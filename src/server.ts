2bc6653936f947dd89fb1d769437755e86a3df3b
David Carboni <david@carboni.io>
2026-03-27 17:41:09 +0000

    Add scheuled call to /apply-terminal-retention

--- src/server.ts
+++ src/server.ts
@@ -88,6 +88,12 @@ cron.schedule('0 6 * * *', async () => {
   // Retention cron jobs
 
   try {
+    await axios.post(`http://localhost:${config.gullsApiPort}${config.pathPrefix}/apply-terminal-retention`);
+  } catch (error: unknown) {
+    console.error(JsonUtils.unErrorJson(error));
+  }
+
+  try {
     await axios.post(`http://localhost:${config.gullsApiPort}${config.pathPrefix}/apply-undetermined-retention`);
   } catch (error: unknown) {
     console.error(JsonUtils.unErrorJson(error));
